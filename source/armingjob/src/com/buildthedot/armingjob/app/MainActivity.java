f32f2076b06c5efdef706c551b761fb2106833b0
nikom2532 <arming@buildthedot.com>
2014-02-18 00:05:47 +0700

    .

--- source/armingjob/src/com/buildthedot/armingjob/app/MainActivity.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/MainActivity.java
@@ -5,15 +5,33 @@ import com.buildthedot.armingjob.R.layout;
 import com.buildthedot.armingjob.R.menu;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	int SPLASH_TIME_OUT = 2000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Intent intent = new Intent(this, ResetPasswordForgetquestionInternational.class);
+		// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		// startActivity(intent);
+		// finish();
+		
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent i = new Intent(MainActivity.this, MainMenu.class);
+				startActivity(i);
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
 	}
 
 	@Override
--- source/armingjob/src/com/buildthedot/armingjob/app/SplashScreen.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.buildthedot.armingjob.app;
 
+import com.buildthedot.armingjob.R;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,9 +13,7 @@ public class SplashScreen extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.splashscreen);
-
-//		setView();
+		setView();
 		
 		// For View in another activity for development
 
@@ -21,33 +21,20 @@ public class SplashScreen extends Activity {
 		// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 		// startActivity(intent);
 		// finish();
+		
+		new Handler().postDelayed(new Runnable() {
 
-//		new Handler().postDelayed(new Runnable() {
-//
-//			@Override
-//			public void run() {
-////				Intent i = new Intent(SplashScreen.this, MainEFilling.class);
-////				Intent i = new Intent(SplashScreen.this, TermsAndConditions.class);
-////				Intent i = new Intent(SplashScreen.this, InstructionsList.class);
-////				Intent i = new Intent(SplashScreen.this, RegisterStep1.class);
-////				Intent i = new Intent(SplashScreen.this, RegisterStep3.class);
-////				Intent i = new Intent(SplashScreen.this, ResetPasswordStep1.class);
-////				Intent i = new Intent(SplashScreen.this, ResetPasswordHaveEmailRequest.class);
-////				Intent i = new Intent(SplashScreen.this, ResetPasswordNoHaveEmailRequest.class);
-////				Intent i = new Intent(SplashScreen.this, ResetPasswordNoHaveEmailRequestStep2.class);
-////				Intent i = new Intent(SplashScreen.this, ResetPasswordForgetquestionThai.class);
-////				Intent i = new Intent(SplashScreen.this, ResetPasswordWithNid.class);
-////				Intent i = new Intent(SplashScreen.this, ResultFilling.class);
-////				Intent i = new Intent(SplashScreen.this, UpdateProfile.class);
-////				Intent i = new Intent(SplashScreen.this, Login_E_Filing.class);
-//				startActivity(i);
-//				finish();
-//			}
-//		}, SPLASH_TIME_OUT);
+			@Override
+			public void run() {
+				Intent i = new Intent(SplashScreen.this, MainMenu.class);
+				startActivity(i);
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
 		
 	}
 
 	void setView() {
-//		setContentView(R.layout.splashscreen);
+		setContentView(R.layout.splashscreen);
 	}
 }
