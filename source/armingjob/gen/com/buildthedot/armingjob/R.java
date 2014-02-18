03ca2279b1f8f166d467ea08cfa6d68f602b495a
arming <nikom2532@gmail.com>
2014-02-18 18:24:48 +0700

    mainmenu frontend layout

--- source/armingjob/gen/com/buildthedot/armingjob/R.java
+++ source/armingjob/gen/com/buildthedot/armingjob/R.java
@@ -21,13 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int i_ming=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int armingjob_mainmenu_login=0x7f080000;
+        public static final int armingjob_mainmenu_register=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int mainmenu=0x7f030001;
+        public static final int splashscreen=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,6 +40,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int armingjob_mainmenu_login=0x7f050003;
+        public static final int armingjob_mainmenu_register=0x7f050004;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
