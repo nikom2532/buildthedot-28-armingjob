c28ad23170f89cd9ebdb2e87c2ed74de275abc33
nikom2532 <arming@buildthedot.com>
2014-02-19 11:01:48 +0700

    layout login

---
+++ source/armingjob/src/com/buildthedot/armingjob/app/Login.java
@@ -0,0 +1,17 @@
+package com.buildthedot.armingjob.app;
+
+import com.buildthedot.armingjob.R;
+import com.buildthedot.armingjob.R.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Login extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainmenu);
+	}
+
+}
