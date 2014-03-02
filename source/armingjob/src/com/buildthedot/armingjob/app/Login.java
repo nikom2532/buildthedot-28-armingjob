b5e02342465cf295cd8280a0e6b07e06619b5202
nikom2532 <arming@buildthedot.com>
2014-03-03 01:40:19 +0700

    .

--- source/armingjob/src/com/buildthedot/armingjob/app/Login.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/Login.java
@@ -11,7 +11,7 @@ public class Login extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.mainmenu);
+		setContentView(R.layout.login);
 	}
 
 }
