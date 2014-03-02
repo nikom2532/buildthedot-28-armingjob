48d7521f7a11a27d2aa4da651f30cac0d1682d2a
Arming Huang <nikom2532@gmail.com>
2014-03-02 17:15:13 +0700

    Add the action link

--- source/armingjob/src/com/buildthedot/armingjob/app/MainMenu.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/MainMenu.java
@@ -4,14 +4,22 @@ import com.buildthedot.armingjob.R;
 import com.buildthedot.armingjob.R.layout;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.app.Activity;
 
 public class MainMenu extends Activity {
+	
+	Button armingjob_mainmenu_login;
+	Button armingjob_mainmenu_register;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainmenu);
+	}
+	private void onclick() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
