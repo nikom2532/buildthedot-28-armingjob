14bd1ff277a620cbe648ed1721673785d625428a
nikom2532 <arming@buildthedot.com>
2014-03-03 16:19:10 +0700

    add button actions

--- source/armingjob/src/com/buildthedot/armingjob/app/MainMenu.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/MainMenu.java
@@ -1,15 +1,16 @@
 package com.buildthedot.armingjob.app;
 
 import com.buildthedot.armingjob.R;
-import com.buildthedot.armingjob.R.layout;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.app.Activity;
 import android.content.Intent;
 
-public class MainMenu extends Activity {
+public class MainMenu extends Activity implements OnClickListener {
 	
 	Button armingjob_mainmenu_login;
 	Button armingjob_mainmenu_register;
@@ -23,10 +24,16 @@ public class MainMenu extends Activity {
 	void setView(){
 		armingjob_mainmenu_login = (Button) findViewById(R.id.armingjob_mainmenu_login);
 		armingjob_mainmenu_register = (Button) findViewById(R.id.armingjob_mainmenu_register);
+		armingjob_mainmenu_login.setOnClickListener(this);
+		armingjob_mainmenu_register.setOnClickListener(this);
 	}
-	public void onclick(View v) {
+	
+	@Override
+	public void onClick(View v) {
+		Log.v("login labelaaa", armingjob_mainmenu_login.getText().toString());
 		// TODO Auto-generated method stub
 		if(armingjob_mainmenu_login.equals(v)){
+			Log.v("login label", armingjob_mainmenu_login.getText().toString());
 			Intent intent = new Intent(this, Login.class);
 			intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 			startActivity(intent);
