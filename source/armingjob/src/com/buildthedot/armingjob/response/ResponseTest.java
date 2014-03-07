b5a1ce68fe77f5d91bcb6a5f8659b3cb908ad7f6
arming <nikom2532@gmail.com>
2014-03-08 00:45:54 +0700

    .

---
+++ source/armingjob/src/com/buildthedot/armingjob/response/ResponseTest.java
@@ -0,0 +1,14 @@
+package com.buildthedot.armingjob.response;
+
+import java.util.ArrayList;
+import com.buildthedot.armingjob.response.ResponseTestCountry;
+
+public class ResponseTest {
+	
+	ArrayList<ResponseTestCountry> data;
+	 
+	public ResponseTest() {
+		data = new ArrayList<ResponseTestCountry>();
+	}
+	 
+}
---
+++ source/armingjob/src/com/buildthedot/armingjob/response/ResponseTestCountry.java
@@ -0,0 +1,21 @@
+package com.buildthedot.armingjob.response;
+
+public class ResponseTestCountry {
+	 
+	 String country;
+	 String capital;
+	 
+	 public ResponseTestCountry() {
+	 
+		 this.country = "";
+		 this.capital = "";
+	 }
+	 
+	 public String getCountry() {
+		 return this.country;
+	 }
+	 
+	 public String getCapital() {
+		 return this.capital;
+	 }
+}
