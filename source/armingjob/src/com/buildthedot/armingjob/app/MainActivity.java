9a47c5cffb2c44b4588a1b07effd27a962c567a0
arming <nikom2532@gmail.com>
2014-03-12 15:43:29 +0700

    add findjob page

--- source/armingjob/src/com/buildthedot/armingjob/app/MainActivity.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/MainActivity.java
@@ -1,37 +1,16 @@
 package com.buildthedot.armingjob.app;
 
 import com.buildthedot.armingjob.R;
-import com.buildthedot.armingjob.R.layout;
-import com.buildthedot.armingjob.R.menu;
-
 import android.os.Bundle;
-import android.os.Handler;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-	int SPLASH_TIME_OUT = 2000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		// Intent intent = new Intent(this, ResetPasswordForgetquestionInternational.class);
-		// intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-		// startActivity(intent);
-		// finish();
-		
-//		new Handler().postDelayed(new Runnable() {
-//
-//			@Override
-//			public void run() {
-//				Intent i = new Intent(MainActivity.this, MainMenu.class);
-//				startActivity(i);
-//				finish();
-//			}
-//		}, SPLASH_TIME_OUT);
 	}
 
 	@Override
@@ -40,5 +19,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
