a5c07d074f98ec90325eff5d886e346dc9e1ce8a
arming <nikom2532@gmail.com>
2014-03-13 15:33:59 +0700

    Android: add ListView and test

--- source/armingjob/src/com/buildthedot/armingjob/app/FindJob.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/FindJob.java
@@ -1,21 +1,36 @@
 package com.buildthedot.armingjob.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.buildthedot.armingjob.R;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import com.buildthedot.armingjob.function.FindjobListAdapter;
 
 public class FindJob extends Activity  {
 	
-	final static int ELEMENT_COUNT = 400;
-
+	public class codeLeanChapter {
+		String chapterName;
+		String chapterDescription;
+	}
+	CodeLearnAdapter chapterListAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,18 +38,87 @@ public class FindJob extends Activity  {
 		setView();
 	}
 	void setView(){
-		String[] elements = new String[ELEMENT_COUNT];
+		chapterListAdapter = new CodeLearnAdapter();
         
-        for (int i = 0; i< ELEMENT_COUNT; i++) {
-        	elements[i] = String.valueOf(i);
-        }
+        ListView codeLearnLessons = (ListView)findViewById(R.id.listView1);
+        codeLearnLessons.setAdapter(chapterListAdapter);
         
-        FindjobListAdapter adapter = new FindjobListAdapter(this,elements);
+        codeLearnLessons.setOnItemClickListener(new OnItemClickListener() {
 
-        final ListView list = (ListView) findViewById(R.id.list);
-        list.setDivider( null ); 
-        list.setAdapter(adapter);
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				
+				codeLeanChapter chapter = chapterListAdapter.getCodeLearnChapter(arg2);
+				
+				Toast.makeText(FindJob.this, chapter.chapterName,Toast.LENGTH_LONG).show();
+				
+			}
+		});
 	}
 	
+	public class CodeLearnAdapter extends BaseAdapter {
 
+    	List<codeLeanChapter> codeLeanChapterList = getDataForListView();
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return codeLeanChapterList.size();
+		}
+
+		@Override
+		public codeLeanChapter getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return codeLeanChapterList.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			
+			if(arg1==null)
+			{
+				LayoutInflater inflater = (LayoutInflater) FindJob.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				arg1 = inflater.inflate(R.layout.findjoblist, arg2,false);
+			}
+			
+			TextView chapterName = (TextView)arg1.findViewById(R.id.armingjob_findjob_title_titlejob);
+			TextView chapterDesc = (TextView)arg1.findViewById(R.id.armingjob_findjob_value_titlejob);
+			
+			codeLeanChapter chapter = codeLeanChapterList.get(arg0);
+			
+			chapterName.setText(chapter.chapterName);
+			chapterDesc.setText(chapter.chapterDescription);
+			
+			return arg1;
+		}
+		
+		public codeLeanChapter getCodeLearnChapter(int position)
+		{
+			return codeLeanChapterList.get(position);
+		}
+
+    }
+	
+	public List<codeLeanChapter> getDataForListView()
+    {
+    	List<codeLeanChapter> codeLeanChaptersList = new ArrayList<codeLeanChapter>();
+    	
+    	for(int i=0;i<10;i++)
+    	{
+    		
+    		codeLeanChapter chapter = new codeLeanChapter();
+    		chapter.chapterName = "Chapter "+i;
+    		chapter.chapterDescription = "This is description for chapter "+i;
+    		codeLeanChaptersList.add(chapter);
+    	}
+    	
+    	return codeLeanChaptersList;
+    	
+    }
 }
--- source/armingjob/src/com/buildthedot/armingjob/app/HomePage.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/HomePage.java
@@ -12,9 +12,6 @@ import android.content.Intent;
 
 public class HomePage extends Activity implements OnClickListener {
 	
-	Button armingjob_mainmenu_login;
-	Button armingjob_mainmenu_register;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,25 +19,10 @@ public class HomePage extends Activity implements OnClickListener {
 		setView();
 	}
 	void setView(){
-		armingjob_mainmenu_login = (Button) findViewById(R.id.armingjob_mainmenu_login);
-		armingjob_mainmenu_register = (Button) findViewById(R.id.armingjob_mainmenu_register);
-		armingjob_mainmenu_login.setOnClickListener(this);
-		armingjob_mainmenu_register.setOnClickListener(this);
 	}
 	
 	@Override
 	public void onClick(View v) {
-		if(armingjob_mainmenu_login.equals(v)){
-			Log.v("login label", armingjob_mainmenu_login.getText().toString());
-			Intent intent = new Intent(this, Login.class);
-			intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-			startActivity(intent);
-		}
-		else if(armingjob_mainmenu_register.equals(v)){
-			Intent intent = new Intent(this, Login.class);
-			intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-			startActivity(intent);
-		}
 	}
 
 }
