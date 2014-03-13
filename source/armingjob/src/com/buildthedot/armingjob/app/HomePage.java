1d662bfaeeb931ca63228e2b50832953810b807e
arming <nikom2532@gmail.com>
2014-03-13 20:05:54 +0700

    Add onclick listener HomePage

--- source/armingjob/src/com/buildthedot/armingjob/app/HomePage.java
+++ source/armingjob/src/com/buildthedot/armingjob/app/HomePage.java
@@ -12,6 +12,9 @@ import android.content.Intent;
 
 public class HomePage extends Activity implements OnClickListener {
 	
+	Button armingjob_mainmenu_login;
+	Button armingjob_mainmenu_register;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,10 +22,24 @@ public class HomePage extends Activity implements OnClickListener {
 		setView();
 	}
 	void setView(){
+		armingjob_mainmenu_login = (Button) findViewById(R.id.armingjob_mainmenu_login);
+		armingjob_mainmenu_register = (Button) findViewById(R.id.armingjob_mainmenu_register);
+		armingjob_mainmenu_login.setOnClickListener(this);
+		armingjob_mainmenu_register.setOnClickListener(this);
 	}
 	
 	@Override
 	public void onClick(View v) {
+		if(armingjob_mainmenu_login.equals(v)){
+			Intent i = new Intent(HomePage.this, Login.class);
+			startActivity(i);
+			finish();
+		}
+		else if(armingjob_mainmenu_register.equals(v)){
+			Intent i = new Intent(HomePage.this, Login.class);
+			startActivity(i);
+			finish();
+		}
 	}
 
 }
---
+++ source/armingjob/src/com/buildthedot/armingjob/response/ResponseFindJobDefault.java
@@ -0,0 +1,8 @@
+package com.buildthedot.armingjob.response;
+
+public class ResponseFindJobDefault {
+	public String jobID = "";
+	public String CompanyID = "";
+	public String position_thai = "";
+	public String salary = "";
+}
